0006bbadc17729e8e5db30222ae403725901d3ce
sagorahamed1 <sagorahammed002@gmail.com>
2023-12-22 14:32:10 +0600

    final

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CheckoutScreen(),
+      home: HomeScreen(),
       //   home: SearchScreen(),
       // home: DetailsProductScreen(),
       //  home: YourCartScreen(),
